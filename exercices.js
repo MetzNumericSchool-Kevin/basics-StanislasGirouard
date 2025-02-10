67bd0f164858707fe8d4b460e8040782074db12a
stanislas <stanislas.girouard@gmail.com>
2025-02-10 14:32:37 +0100

    affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -3,3 +3,9 @@ const shopName = "GreenBean"
 let nbPotion = 3
 let healthPotionPrice = 5
 let isOpen = true
+
+if (isOpen){
+    console.log("Bienvenue dans la boutique " + shopName + " ! 🎉");
+}else{
+    console.log("La boutique " + shopName + " est fermée, revenez plus tard !")
+}
